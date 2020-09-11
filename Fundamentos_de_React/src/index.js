a88bbea88325614b919bdbbff8274c688c269001
Manuel Roberto Serrano Torres <CastielAltair0027@outlook.com>
2020-09-11 02:26:45 -0500

    H&&H

--- Fundamentos_de_React/src/index.js
+++ Fundamentos_de_React/src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 // import HelloWorld from './components/HelloWorld';
 import App from './containers/App';
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'));
